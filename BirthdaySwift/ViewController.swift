471f50f35fd855d5ed4cf1c864e3488cad1c6f13
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-23 20:18:39 +0600

    works user defaults

--- BirthdaySwift/ViewController.swift
+++ BirthdaySwift/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirth = UserDefaults.standard.object(forKey: "birth")
+        
+        if let newName = storedName as? String {
+            nameLabel.text = newName
+        }
+        if let newBirth = storedBirth as? String {
+            birthdayLabel.text = newBirth
+        }
     }
 
-
+    @IBAction func saveButton(_ sender: Any) {
+        UserDefaults.standard.set(nameText.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayText.text!, forKey: "birth")
+        
+        nameLabel.text = "Name: \(nameText.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayText.text!)"
+    }
+    
 }
 
